f222e9a059cc101384e84f68ebd7ce4c46aa3b84
alitrk <turkali750@gmail.com>
2023-05-18 15:13:03 +0300

    NotesScreen ui and AddEditViewModel created

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/add_edit_note/AddEditNoteViewModel.kt
@@ -0,0 +1,12 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.add_edit_note
+
+import androidx.lifecycle.ViewModel
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case.NoteUseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AddEditNoteViewModel @Inject constructor(
+    private val noteUseCases: NoteUseCases
+) : ViewModel() {
+}
